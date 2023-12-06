1a820a7ef5b6723213ea44d8e8f274e7fcd5fb68
KuroPika04 <justintjitra@gmail.com>
2023-12-06 19:13:57 +0700

    initial commit

--- Paracelsus/Assets/Scripts/PlayerMovement.cs
+++ Paracelsus/Assets/Scripts/PlayerMovement.cs
@@ -13,9 +13,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontal_input = Input.GetAxis("Horizontal");
 
-        if (Input.GetKey(KeyCode.Space))
+        body.velocity = new Vector2(horizontal_input * speed, body.velocity.y);
+
+        if (horizontal_input > 0.01)
+            transform.localScale = new Vector3(3, 3, 3);
+        else if (horizontal_input < -0.01)
+            transform.localScale = new Vector3(-3, 3, 3);
+
+        if (Input.GetKeyDown(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
     }
 }
