02972bacf267a33df823f62957b808c65abe9f3a
KuroPika04 <justintjitra@gmail.com>
2023-12-06 20:55:37 +0700

    initial commit

--- Paracelsus/Assets/Scripts/PlayerMovement.cs
+++ Paracelsus/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -22,7 +25,21 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontal_input < -0.01)
             transform.localScale = new Vector3(-3, 3, 3);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+            Jump();
+
+        anim.SetBool("run", horizontal_input != 0);
+        anim.SetBool("grounded", grounded);
+    }
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
