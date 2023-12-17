2b6f68dcf768fb48a835cbe6c897a3e50ea3135b
KuroPika04 <justintjitra@gmail.com>
2023-12-17 22:44:02 +0700

    double jump

--- Paracelsus/Assets/Scripts/PlayerMovement.cs
+++ Paracelsus/Assets/Scripts/PlayerMovement.cs
@@ -14,14 +14,13 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D body;
     private Animator anim;
-    //private bool grounded;
     private float coyote_time = 0.2f;
     private float coyote_counter;
     private float jumpbuffer_time = 0.2f;
     private float jumpbuffer_counter;
     private bool can_dash = true;
     private bool currently_dash;
-    private bool double_jump;
+    private int extra_jump = 1;
 
     private void Awake()
     {
@@ -43,6 +42,7 @@ public class PlayerMovement : MonoBehaviour
         if (grounded())
         {
             coyote_counter = coyote_time;
+            extra_jump = 1;
         }
         else
         {
@@ -63,8 +63,15 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontal_input < -0.01)
             transform.localScale = new Vector3(-3, 3, 3);
 
-        if (jumpbuffer_counter > 0f && coyote_counter > 0f)
-            Jump();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpbuffer_counter > 0f && coyote_counter > 0)
+                Jump();
+            else if (extra_jump > 0)
+                Jump();
+            extra_jump--;
+        }
+        
         //if (Input.GetKeyUp(KeyCode.Space) && body.velocity.y > 0f)
         //    ReleaseJump();
 
@@ -78,12 +85,11 @@ public class PlayerMovement : MonoBehaviour
     {
         anim.SetTrigger("jump");
         body.velocity = new Vector2(body.velocity.x, jump_power);
-        
-        //grounded = false;
         //double_jump = !double_jump;
         coyote_counter = 0f;
         jumpbuffer_counter = 0f;
     }
+
     //private void ReleaseJump()
     //{
     //    body.velocity = new Vector2(body.velocity.x, body.velocity.y * 0.5f);
@@ -93,11 +99,6 @@ public class PlayerMovement : MonoBehaviour
     {
         return Physics2D.OverlapCircle(ground_check.position, 0.2f, ground_layer);
     }
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.tag == "Ground")
-    //        grounded = true;
-    //}
     private IEnumerator Dash()
     {
         can_dash = false;
