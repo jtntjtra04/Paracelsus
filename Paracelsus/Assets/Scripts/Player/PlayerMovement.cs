2c08cc88bacd3c27e2e3a9ef7fcd0378378193a7
Brynkkk <bryankurniawan97@gmail.com>
2023-12-30 01:05:45 +0700

    Checkpoint hp regen + Floor 1 map
    if you interact with the checkpoint while hp < 3, then your hp will increase until 3.

--- Paracelsus/Assets/Scripts/Player/PlayerMovement.cs
+++ Paracelsus/Assets/Scripts/Player/PlayerMovement.cs
@@ -62,9 +62,9 @@ public class PlayerMovement : MonoBehaviour
         }
 
         if (horizontal_input > 0.01)
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(0.37f, 0.37f, 0.37f);
         else if (horizontal_input < -0.01)
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-0.37f, 0.37f, 0.37f);
 
         if (Input.GetKey(KeyCode.Space) && body.velocity.y <= 0)  // glide
         {
