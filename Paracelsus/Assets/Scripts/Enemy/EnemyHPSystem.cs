b04888bf633c263234c0f5c1a6f3069f96ab7d4e
KuroPika04 <justintjitra@gmail.com>
2024-02-24 23:32:07 +0700

    SKELETON DEFEAT ANIMATION AND SYSTEM

--- Paracelsus/Assets/Scripts/Enemy/EnemyHPSystem.cs
+++ Paracelsus/Assets/Scripts/Enemy/EnemyHPSystem.cs
@@ -6,21 +6,66 @@ public class EnemyHPSystem : MonoBehaviour
 {
     public float health;
     public float curr_health;
+    private bool isDefeat = false;
+
+    // References
+    private Rigidbody2D body;
+    private SkeletonAI skeleton_attack;
+    private SkeletonPatrol skeleton_movement;
     public EnemyHealthBar hp_bar;
+    private Animator anim;
     private void Start()
     {
+        anim = GetComponent<Animator>();
+        skeleton_attack = GetComponent<SkeletonAI>();
+        skeleton_movement = GetComponentInParent<SkeletonPatrol>();
         curr_health = health;
         hp_bar.SetHealth(curr_health, health);
+        body = GetComponent<Rigidbody2D>();
+        if(skeleton_movement == null)
+        {
+            Debug.Log("Movement null raaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        }
+        if(skeleton_attack == null) 
+        {
+            Debug.Log("Attack null raaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        }
     }
     public void EnemyTakeDamage(float damage)
     {
-        curr_health -= damage;
-        hp_bar.SetHealth(curr_health, health);
-
-        if (curr_health <= 0)
+        if (!isDefeat)
         {
-            Die();
+            curr_health -= damage;
+            hp_bar.SetHealth(curr_health, health);
+
+            if (curr_health <= 0)
+            {
+                StartCoroutine(DefeatAnimation());
+            }
         }
+    }
+    private IEnumerator DefeatAnimation()
+    {
+        isDefeat = true;
+        anim.SetTrigger("Defeat");
+
+        hp_bar.gameObject.SetActive(false);
+
+        if (skeleton_movement != null)
+        {
+            Debug.Log("Disabled movement");
+            skeleton_movement.enabled = false;
+            body.constraints = RigidbodyConstraints2D.FreezePositionX;
+        }
+        if (skeleton_attack != null)
+        {
+            Debug.Log("Disables attack");
+            skeleton_attack.enabled = false;
+        }
+        
+        yield return new WaitForSeconds(1.5f);
+
+        Die();
     }
     public void Die()
     {
