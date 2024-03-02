e3d612f23ffcd0ecf0a236d095eae788a443da5a
KuroPika04 <justintjitra@gmail.com>
2024-03-03 00:04:03 +0700

    All slime movement disabled when ded

--- Paracelsus/Assets/Scripts/Enemy/EnemyHPSystem.cs
+++ Paracelsus/Assets/Scripts/Enemy/EnemyHPSystem.cs
@@ -10,9 +10,15 @@ public class EnemyHPSystem : MonoBehaviour
 
     // References
     private Rigidbody2D body;
-    private SkeletonAI skeleton_attack;
-    private SkeletonPatrol skeleton_movement;
-    private SlimeAI slime_movement;
+    private BoxCollider2D box_collider;
+    private SkeletonAI skeleton_attack; // skeleton
+    private SkeletonPatrol skeleton_movement; // skeleton
+    private SlimeAI slime_movement; // slime movement 1, 3
+    private SlimeAI2wp slime_movement_2; // slime movement 2, 6
+    private SlimeAIWS4 slime_movement_4; // slime movement 4
+    private SlimeAIWS5 slime_movement_5; // slime movement 5
+    private SlimeAIWS7 slime_movement_7; // slime movement 7
+    private SlimeAIWS8 slime_movement_8; // slime movement 8
     public EnemyHealthBar hp_bar;
     private Animator anim;
 
@@ -26,9 +32,15 @@ public class EnemyHPSystem : MonoBehaviour
         skeleton_attack = GetComponent<SkeletonAI>();
         skeleton_movement = GetComponentInParent<SkeletonPatrol>();
         slime_movement = GetComponent<SlimeAI>();
+        slime_movement_2 = GetComponent<SlimeAI2wp>();
+        slime_movement_4 = GetComponent<SlimeAIWS4>();
+        slime_movement_5 = GetComponent<SlimeAIWS5>();
+        slime_movement_7 = GetComponent<SlimeAIWS7>();
+        slime_movement_8 = GetComponent<SlimeAIWS8>();
         curr_health = health;
         hp_bar.SetHealth(curr_health, health);
         body = GetComponent<Rigidbody2D>();
+        box_collider = GetComponent<BoxCollider2D>();
     }
     public void EnemyTakeDamage(float damage)
     {
@@ -69,10 +81,35 @@ public class EnemyHPSystem : MonoBehaviour
         }
         if (slime_movement != null)
         {
-            Debug.Log("Disabled slime movement");
             slime_movement.enabled = false;
+            box_collider.enabled = false;
         }
-        
+        if (slime_movement_2 != null)
+        {
+            slime_movement_2.enabled = false;
+            box_collider.enabled = false;
+        }
+        if (slime_movement_4 != null)
+        {
+            slime_movement_4.enabled = false;
+            box_collider.enabled = false;
+        }
+        if (slime_movement_5 != null)
+        {
+            slime_movement_5.enabled = false;
+            box_collider.enabled = false;
+        }
+        if (slime_movement_7 != null)
+        {
+            slime_movement_7.enabled = false;
+            box_collider.enabled = false;
+        }
+        if (slime_movement_8 != null)
+        {
+            slime_movement_8.enabled = false;
+            box_collider.enabled = false;
+        }
+
         yield return new WaitForSeconds(1.5f);
 
         Die();
