a9a68cdd2b0b48bc482e03d02376729be1712cfa
triscakepuff <tristanchandra90@gmail.com>
2024-03-06 13:36:30 +0700

    Shotgun Skill (Complete)
    kalo mau diotak"in brp bullet yg ke-instantiate bisa ganti di numberofBullets dari script switchSkills
    Kalo damagenya bisa diganti dari script prefab bullet shotgun
    silakan dilihat dl sblm gw push

--- Paracelsus/Assets/Scripts/Skills/shotgunSkill.cs
+++ Paracelsus/Assets/Scripts/Skills/shotgunSkill.cs
@@ -15,40 +15,25 @@ public class shotgunSkill : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // float playerScaleX = player.transform.localScale.x;
-        // if(playerScaleX < 0)
-        // {
-        //     Vector2 pelletScaleX = -transform.right;
-        // }else
-        // {
-        //     Vector2 pelletScaleX = transform.right;
-        // }
-        void OnTriggerEnter2D(Collider2D collision)
-        {
-            Debug.Log(collision.tag);
-        EnemyHPSystem enemy_hp = collision.GetComponent<EnemyHPSystem>();
         
-
-         if (enemy_hp != null)
-         {
-            string projectile_tag = gameObject.tag;
-         if (collision.CompareTag("FireEnemy"))
-        {
-            enemy_hp.EnemyTakeDamage(damage * 0.75f);
-        }
-        else if (collision.CompareTag("WindEnemy") )
-        {
-            enemy_hp.EnemyTakeDamage(damage * 1.5f);
-        }
-        else if (collision.CompareTag("FireSlime") )
-        {
-            enemy_hp.EnemyTakeDamage(damage * 0.75f);
-        }
-        else if (collision.CompareTag("WindSlime"))
-        {
-            enemy_hp.EnemyTakeDamage(damage * 1.5f);
-        }
-         }
-        } 
+        
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+        {
+          
+            EnemyHPSystem enemy_hp = collision.GetComponent<EnemyHPSystem>();
+            
+              if (enemy_hp != null)
+            {
+            if (collision.CompareTag("FireEnemy"))
+            {
+                enemy_hp.EnemyTakeDamage(damage * 0.75f);
+            }
+            else if (collision.CompareTag("WindEnemy"))
+            {
+                enemy_hp.EnemyTakeDamage(damage * 1.5f);
+            }
+            }
+        } 
 }
