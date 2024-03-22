76b873437ed61d395db528c10b474cf8b03b00fc
Brynkkk <bryankurniawan97@gmail.com>
2024-03-22 07:59:40 +0700

    Pickable Potions
    - Added pickable potions
    - fixed parallax problem
    - fixed slime debug problem

---
+++ Paracelsus/Assets/Scripts/Game/HP_Potion.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP_Potion : MonoBehaviour
+{
+    private float maxPot = 5f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameController gameController = GameObject.FindFirstObjectByType<GameController>();
+
+            if (gameController.hpPotion < maxPot)
+            {
+                gameController.hpPotion++;
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("Player already has the maximum number of potions!");
+            }
+        }
+    }
+}
